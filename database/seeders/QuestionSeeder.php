e3849993d32c94e932e5eb0ad02936b8145cbc27
A k <Amani10-c@github.com>
2025-10-05 03:06:48 +0300

    I made the seeder files for the collection, categories and questions

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,34 @@ class QuestionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $folder_path = database_path('data');
+
+        $file = glob($folder_path . '/*.csv');
+
+        foreach ($file as $path) {
+            echo "Importing: " . basename($path) . "/n";
+
+            if (($handle = fopen($path, 'r')) !== false) {
+                $header = fgetcsv($handle, 1000, ',');
+
+                while (($row = fgetcsv($handle, 1000, ',')) != false) {
+                    $data = array_combine($header, $row);
+                    DB::table('questions')->insert([
+                        'category_id' => $data['category_id'],
+                        'type_question' => $data['type_question'],
+                        'answer' => $data['answer'],
+                        'point' => $data['point'],
+                        'difficulty_level' => $data['difficulty_level'],
+                        'question_text' => $data['question_text'],
+                        'image_path' => $data['image_path'] ?? null,
+                        'created_at'=>now(),
+                        'updated_at'=>now()
+
+
+                    ]);
+                }
+                fclose($handle);
+            }
+        }
     }
 }
